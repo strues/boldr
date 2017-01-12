cb2635debb0afc53de49d02bafbed8378ac2f6d1
Steven Truesdell <steven@strues.io>
2017-01-12 11:29:03 -0700

    update dependencies. modify cricle.yml

--- packages/boldr-cms/src/shared/core/api/apiClient.js
+++ packages/boldr-cms/src/shared/core/api/apiClient.js
@@ -9,17 +9,18 @@ function formatUrl(path) {
   if (process.env.NODE_ENV !== 'test') {
     if (__SERVER__) {
       console.log('----SERVER REQUEST----');
-    // Prepend host and port of the API server to the path.
-      return `http://${process.env.API_HOST}:${process.env.API_PORT + adjustedPath}`;
+      // Prepend host and port of the API server to the path.
+      return `http://${process.env.API_HOST}:${process.env.API_PORT +
+        adjustedPath}`;
     }
   }
   console.log('----CLIENT REQUEST----');
-   // Prepend `/api` to relative URL, to proxy to API server.
+  // Prepend `/api` to relative URL, to proxy to API server.
   return `/api/v1${adjustedPath}`;
 }
 
 function clean(obj) {
-  Object.keys(obj).forEach((key) => {
+  Object.keys(obj).forEach(key => {
     if (obj[key] === null || obj[key] === undefined) {
       Reflect.deleteProperty(obj, key);
     }
@@ -29,40 +30,44 @@ function clean(obj) {
 export default class ApiClient {
   constructor(host, req) {
     host = host || '';
-    methods.forEach((method) => {
-      this[method] = (path, { params, data, headers, files, fields } = {}) => new Promise((resolve, reject) => {
-        const request = superagent[method](formatUrl(path));
+    methods.forEach(method => {
+      this[method] = (path, { params, data, headers, files, fields } = {}) =>
+        new Promise((resolve, reject) => {
+          const request = superagent[method](formatUrl(path));
 
-        if (params) {
-          request.query(params);
-        }
+          if (params) {
+            request.query(params);
+          }
 
-        if (headers) {
-          request.set(headers);
-        }
+          if (headers) {
+            request.set(headers);
+          }
 
-        if (typeof window !== 'undefined') {
-          const token = getToken();
+          if (typeof window !== 'undefined') {
+            const token = getToken();
 
-          request.set('Authorization', `Bearer ${token}`);
-        }
+            request.set('Authorization', `Bearer ${token}`);
+          }
 
-        if (files) {
-          files.forEach(file => request.attach(file.key, file.value));
-        }
+          if (files) {
+            files.forEach(file => request.attach(file.key, file.value));
+          }
 
-        if (fields) {
-          fields.forEach(item => request.field(item.key, item.value));
-        }
+          if (fields) {
+            fields.forEach(item => request.field(item.key, item.value));
+          }
 
-        if (data) {
-          clean(data);
-          request.set('Content-Type', 'application/json');
-          request.send(data);
-        }
+          if (data) {
+            clean(data);
+            request.set('Content-Type', 'application/json');
+            request.send(data);
+          }
 
-        request.end((error, response) => (error ? reject(response || error) : resolve(response)));
-      });
+          request.end(
+            (error, response) =>
+              error ? reject(response || error) : resolve(response),
+          );
+        });
     });
   }
 
@@ -76,7 +81,6 @@ export default class ApiClient {
    *
    * Remove it at your own risk.
    */
-
   /* eslint-disable class-methods-use-this */
   empty() {}
   /* eslint-enable class-methods-use-this */
--- packages/boldr-cms/src/shared/scenes/Admin/index.js
+++ packages/boldr-cms/src/shared/scenes/Admin/index.js
@@ -7,74 +7,74 @@ export default (store, connect) => {
     component: DashboardContainer,
     indexRoute: {
       getComponent(nextState, cb) {
-        System.import('./Dashboard/Dashboard')
-        .then(loadRoute(cb))
-        .catch(errorLoading);
+        System
+          .import('./Dashboard/Dashboard')
+          .then(loadRoute(cb))
+          .catch(errorLoading);
       },
     },
     childRoutes: [
       {
         path: 'posts',
         getComponent(nextState, cb) {
-          System.import('./Post/PostList/PostListContainer')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System
+            .import('./Post/PostList/PostListContainer')
+            .then(loadRoute(cb))
+            .catch(errorLoading);
         },
       },
       {
         path: 'posts/editor/:slug',
         getComponent(nextState, cb) {
-          System.import('./Post/PostEditor')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System
+            .import('./Post/PostEditor')
+            .then(loadRoute(cb))
+            .catch(errorLoading);
         },
       },
       {
         path: 'posts/new',
         getComponent(nextState, cb) {
-          System.import('./Post/NewPost/NewPostContainer')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System
+            .import('./Post/NewPost/NewPostContainer')
+            .then(loadRoute(cb))
+            .catch(errorLoading);
         },
       },
       {
         path: 'filemanager',
         getComponent(nextState, cb) {
-          System.import('./FileManager')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System
+            .import('./FileManager')
+            .then(loadRoute(cb))
+            .catch(errorLoading);
         },
       },
       {
         path: 'filemanager/:id/editor',
         getComponent(nextState, cb) {
-          System.import('./FileManager/FileEditor')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System
+            .import('./FileManager/FileEditor')
+            .then(loadRoute(cb))
+            .catch(errorLoading);
         },
       },
       {
         path: 'navigation',
         getComponent(nextState, cb) {
-          System.import('./Navigation')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System.import('./Navigation').then(loadRoute(cb)).catch(errorLoading);
         },
       },
       {
         path: 'members',
         getComponent(nextState, cb) {
-          System.import('./Members')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System.import('./Members').then(loadRoute(cb)).catch(errorLoading);
         },
       },
       {
         path: 'templates',
         getComponent(nextState, cb) {
-          System.import('./Templates')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
+          System.import('./Templates').then(loadRoute(cb)).catch(errorLoading);
         },
       },
     ],
