926de0524a9800fde0c46640223aa7d1cabaae0a
Steven Truesdell <steven@strues.io>
2017-02-01 16:51:49 -0700

    fix tag?

--- server/routes/tag/tag.test.js
+++ server/routes/tag/tag.test.js
@@ -5,8 +5,7 @@ function request() {
   return supertest(app);
 }
 
-
-it('GET /tags -- List', async () => {
+test('GET /tags -- List', async () => {
   const { status, body } = await request()
     .get('/api/v1/tags')
     .set('Accept', 'application/json');
