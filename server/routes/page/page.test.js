6d4da7c3d7eb3dcb2d0f0091dadc9d763ceb83ca
Steven Truesdell <steven@strues.io>
2017-02-02 18:46:14 -0700

    again, fix jest?

--- server/routes/page/page.test.js
+++ server/routes/page/page.test.js
@@ -1,13 +1,11 @@
-import supertest from 'supertest';
+import request from 'supertest';
 import app from '../../app';
 
-function request() {
-  return supertest(app);
-}
-
-it('GET /pages -- It should return pages', async () => {
-  const { status } = await request()
+test('GET /pages -- It should return pages', () => {
+  return request(app)
       .get('/api/v1/pages')
-      .set('Accept', 'application/json');
-  expect(status).toBe(200);
+      .set('Accept', 'application/json')
+      .expect((res) => {
+        expect(res.status).toBe(200);
+      });
 });
