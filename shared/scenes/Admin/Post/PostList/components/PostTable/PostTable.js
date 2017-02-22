a6c51fcbb212df027c78ce50e055ce660cf56ca3
Steven Truesdell <steven@strues.io>
2017-02-22 12:15:06 -0700

    Remove griddle due to breaking changes in their release.

--- shared/scenes/Admin/Post/PostList/components/PostTable/PostTable.js
+++ shared/scenes/Admin/Post/PostList/components/PostTable/PostTable.js
@@ -1,13 +1,18 @@
 /* @flow */
 /* eslint-disable react/prop-types */
 /* eslint-disable  react/jsx-no-bind */
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Link from 'react-router/lib/Link';
 import { format } from 'date-fns';
 import Button from 'react-md/lib/Buttons';
 import FontIcon from 'react-md/lib/FontIcons';
-import Griddle from 'griddle-react';
+import DataTable from 'react-md/lib/DataTables/DataTable';
+import TableHeader from 'react-md/lib/DataTables/TableHeader';
+import TableBody from 'react-md/lib/DataTables/TableBody';
+import TableRow from 'react-md/lib/DataTables/TableRow';
+import TableColumn from 'react-md/lib/DataTables/TableColumn';
+import TablePagination from 'react-md/lib/DataTables/TablePagination';
 import Avatar from 'react-md/lib/Avatars';
 
 import type { Post } from '../../../../../../types/models';
@@ -20,106 +25,43 @@ type Props = {
   dispatch: Function,
   data: any,
 };
-const AvatarComp = (props) => {
-  return (
-    <Avatar src={ props.data } role="presentation" />
-  );
-};
-const LinkComp = (props) => {
-  return (
-  <Link to={ `/admin/posts/editor/${props.rowData.slug}` }>{ props.data }</Link>
-  );
-};
-const DateComp = (props) => {
-  return (
-    <span>{ format(props.data, 'MMMM Do YYYY') }</span>
-  );
-};
-const PublishedComp = (props) => {
-  return (
-    <span>{ props.data === true ? 'Published' : 'Draft' }</span>
-  );
-};
 
-const DeleteComp = ({ data, rowData, metadata }) => {
-  return (
-    <Button
-      onClick={ metadata.customComponentMetadata.onClick.bind(null, data, rowData, metadata) }
-      icon
-    >delete_forever</Button>
-  );
-};
-const PostTable = (props: Props) => {
-  const post = props;
-  function transitionPost() {
-    props.dispatch(selectPost(post));
+class PostTable extends Component {
+
+  handleRowClick = (rowIndex, event) => {
+    console.log(rowIndex, event)
   }
-  const rowMetadata = {
-    'bodyCssClassName'(rowData) {
-      return 'md-table-row';
-    },
-  };
-
-  const metaData = [
-    {
-      columnName: 'feature_image',
-      displayName: 'Feature Image',
-      customComponent: AvatarComp,
-      cssClassName: 'md-table-column avatar-column',
-      visible: true,
-      order: 1,
-    }, {
-      columnName: 'title',
-      displayName: 'Title',
-      visible: true,
-      cssClassName: 'md-table-column title-column md-text',
-      customComponent: LinkComp,
-      order: 2,
-    }, {
-      columnName: 'published',
-      displayName: 'Published',
-      cssClassName: 'md-table-column published-column md-text',
-      customComponent: PublishedComp,
-      visible: true,
-      order: 3,
-    }, {
-      columnName: 'featured',
-      displayName: 'Featured',
-      visible: true,
-      cssClassName: 'md-table-column featured-column md-text',
-      order: 4,
-    }, {
-      columnName: 'created_at',
-      displayName: 'Created',
-      customComponent: DateComp,
-      cssClassName: 'md-table-column created-column md-text',
-      visible: true,
-      order: 5,
-    }, {
-      columnName: 'actions',
-      displayName: 'Actions',
-      customComponent: DeleteComp,
-      cssClassName: 'md-table-column delete-column md-text',
-      customComponentMetadata: {
-        onClick: (data, rowData, metadata) => {
-          props.handleDeleteClick(rowData.id);
-        },
-      },
-      visible: true,
-      order: 6,
-    },
-  ];
-  return (
-    <Griddle results={ props.posts } showFilter columnMetadata={ metaData }
-      showSettings columns={ ['feature_image', 'title', 'published', 'featured', 'created_at', 'actions'] }
-      useGriddleStyles={ false }
-      gridClassName="boldr-table__grid"
-      tableClassName="md-data-table" customFormatClassName="boldr-table__format"
-      rowMetadata={ rowMetadata }
-      settingsToggleClassName="settings-button md-inline-block md-btn md-btn--flat md-text--theme-secondary md-ink--secondary md-pointer--hover md-btn--text"
-    />
-
-  );
-};
-
+  render() {
+    const rows = this.props.posts.map(p => (
+      <TableRow key={ p.id } onCheckboxClick={ this.handleRowClick }>
+        <TableColumn style={ { maxWidth: '125px' } }><Avatar src={ p.feature_image } role="presentation" /></TableColumn>
+        <TableColumn><Link to={ `/admin/posts/editor/${p.slug}` }>{ p.title }</Link></TableColumn>
+        <TableColumn>{ p.published ? <span>Published</span> : <span>Draft</span>}</TableColumn>
+        <TableColumn>{ format(p.created_at, 'MMMM Do YYYY') }</TableColumn>
+        <TableColumn>
+          <Button
+            onClick={ () => this.props.handleDeleteClick(p.id) }
+            icon
+          >delete_forever</Button>
+        </TableColumn>
+      </TableRow>
+    ));
+    return (
+      <DataTable baseId="posts">
+        <TableHeader>
+          <TableRow>
+            <TableColumn>Feature Image</TableColumn>
+            <TableColumn>Title</TableColumn>
+            <TableColumn>Status</TableColumn>
+            <TableColumn>Created</TableColumn>
+            <TableColumn>Actions</TableColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows}
+        </TableBody>
+      </DataTable>
+    );
+  }
+}
 export default connect()(PostTable);
