00993c0bf5b9d8e4576288de11b93d182ad099f4
Steven Truesdell <steven@strues.io>
2017-03-16 13:19:05 -0600

    .

--- internal/jest/transform.js
+++ internal/jest/transform.js
@@ -2,10 +2,12 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [
-    'stage-2',
-    ['env', { targets: { node: true } }],
-    'react',
+  presets: ['stage-2', ['env', { targets: { node: true } }], 'react'],
+  plugins: [
+    'transform-decorators-legacy',
+    'syntax-dynamic-import',
+    'dynamic-import-node',
+    'dynamic-import-webpack',
+    'transform-object-rest-spread',
   ],
-  plugins: ['transform-decorators-legacy', 'syntax-dynamic-import', 'transform-object-rest-spread'],
 });
