ae74b0895239e48cddf4e5ee85d02cba720ffc33
Steven Truesdell <steven@strues.io>
2017-04-11 23:38:51 -0600

    fix test

--- src/shared/components/App/App.test.js
+++ src/shared/components/App/App.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import routes from '../../routes';
 import App from './App';
 
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
 describe('<App />', () => {
-  const wrapper = shallow(<App route={ routes } />);
-  it('renders <App /> without breaking', () => {
-    expect(wrapper.find('div').length).toBe(2);
-  });
+  const store = mockStore({});
+  const wrapper = shallow(<Provider store={ store }><App route={ routes } /></Provider>);
   it('renders snapshot', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
