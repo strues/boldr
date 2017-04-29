c357d641761c861cac197d76bac4f07f3f81115c
Steven Truesdell <steven@strues.io>
2017-04-28 23:59:19 -0600

    test(App): fixes test file

--- src/shared/components/App/App.test.js
+++ src/shared/components/App/App.test.js
@@ -10,11 +10,11 @@ const middlewares = [];
 const mockStore = configureStore(middlewares);
 
 describe('<App />', () => {
-  const store = mockStore({});
-  const wrapper = shallow(
-    <Provider store={store}><App route={routes} /></Provider>,
-  );
   it('renders snapshot', () => {
+    const store = mockStore({});
+    const wrapper = shallow(
+      <Provider store={store}><App route={routes} /></Provider>,
+    );
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 });
