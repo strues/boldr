0b31faa546780a883b5bbb5b53cd1cbe02572f3f
Steven Truesdell <steven@strues.io>
2017-05-03 12:57:50 -0600

    chore(package): bump boldr-ui ver

--- internal/utils.js
+++ internal/utils.js
@@ -1,13 +1,17 @@
+import os from 'os';
 import { execSync } from 'child_process';
 import appRootDir from 'app-root-dir';
 
 import HappyPack from 'happypack';
 
 export function happyPackPlugin({ name, loaders }) {
+   const compilerThreadPool = HappyPack.ThreadPool({
+    size: os.cpus().length,
+  });
   return new HappyPack({
     id: name,
     verbose: false,
-    threads: 4,
+    threadPool: compilerThreadPool,
     loaders,
   });
 }
